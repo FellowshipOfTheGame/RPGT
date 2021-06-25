24c5581e722e765c7fecd27d88f38bfafbe6d33c
Luís Chaves <pi.luis.f3@gmail.com>
2021-06-25 19:03:11 -0300

    ui finished

--- Assets/Scripts/Entities/CustomPlayerRoom.cs
+++ Assets/Scripts/Entities/CustomPlayerRoom.cs
@@ -1,11 +1,29 @@
 using Mirror;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CustomPlayerRoom : NetworkRoomPlayer
 {
     public List<Skill> skills = new List<Skill> { new BasicAttack(), new BasicHeal(), new Fireball() };
     public static CustomPlayerRoom localPlayerRoom = null;
+
+    public Text nameText;
+    public Button readyButton;
+    public Text readyText;
+
+    void Awake() {
+        Transform ui = GameObject.Find("UIPanel").transform.Find("Players").transform.Find("ScrollRect").transform.Find("Content (Players)");
+
+        transform.SetParent(ui);
+    }
+
+    /// <summary>Like Start(), but only called on client and host.</summary>
+    public override void OnStartClient() {
+        if (isLocalPlayer) {
+            readyButton.gameObject.SetActive(true);
+        }
+    }
 
     /// <summary>
     /// Called when the local player object has been set up.
@@ -18,4 +36,21 @@ public class CustomPlayerRoom : NetworkRoomPlayer
         }
         localPlayerRoom = this;
     }
+
+    public void ChangeReadyStatus() {
+        CmdChangeReadyState(!readyToBegin);
+    }
+
+    public override void ReadyStateChanged(bool oldReadyState, bool newReadyState) {
+        if (newReadyState) {
+            readyText.text = "Ready";
+            readyText.color = new Color(0, 200, 0);
+        }
+        else {
+            readyText.text = "Not Ready";
+            readyText.color = new Color(200, 0, 0);
+        }
+    }
+
+
 }
